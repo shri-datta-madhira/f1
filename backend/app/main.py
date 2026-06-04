51fe146351be06893f215aa6f986423a0c8af20a
Shri Datta Madhira <madhira.s@northeastern.edu>
2026-06-04 16:10:00 -0500

    Enable CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="F1 Analysis & Prediction Platform",
     description="Historical F1 analysis + race prediction.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
 )
 
 
